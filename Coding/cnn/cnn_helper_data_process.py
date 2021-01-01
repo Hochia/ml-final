c5458a3abf47f1761a3d819e656491176f85e3c0
Hochia <johnny30618@gmail.com>
2021-01-01 14:43:12 +0800

    clean code

--- Coding/cnn/cnn_helper_data_process.py
+++ Coding/cnn/cnn_helper_data_process.py
@@ -1,11 +1,11 @@
 # retrieve one attribute for two categorical training
 def cnnAttr(df, attrs):
   return keras.utils.to_categorical(np.asarray((df[attrs]+1)/2), 2)
-
-df_attr = pd.read_csv('dataset/list_attr_celeba.csv') # 40 attributes
-y=cnnAttr(df_attr, 'Wearing_Lipstick')
-# check
-y.shape
+# # examples
+# df_attr = pd.read_csv('dataset/list_attr_celeba.csv') # 40 attributes
+# y=cnnAttr(df_attr, 'Wearing_Lipstick')
+# # check
+# y.shape
 
 # save Sequential model and its History object
 def cnnSave(model, history, root, name):
@@ -21,14 +21,14 @@ def cnnLoad(root, name):
     model=keras.models.load_model(rn+'.h5')
     history=pd.read_pickle(rn+'.pkl')
     return model, history
-# examples
-model=keras.Sequential([...], 'model_name')
-history=model.fit(x, y, batch_size=batch_size, epochs=epochs, validation_split=0.1)
-## root to save
-root_model_save='Coding/data/model/lipstick'
-# save it
-cnnSave(model, history, root_model_save, 'model_name')
-# delete the objects
-del model, history
-# load for future use
-model,history=cnnLoad(root_model_save, 'model_name')
+# # examples
+# model=keras.Sequential([...], 'model_name')
+# history=model.fit(x, y, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+# ## root to save
+# root_model_save='Coding/data/model/lipstick'
+# # save it
+# cnnSave(model, history, root_model_save, 'model_name')
+# # delete the objects
+# del model, history
+# # load for future use
+# model,history=cnnLoad(root_model_save, 'model_name')
