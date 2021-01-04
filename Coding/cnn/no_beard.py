f06c9b183e28a1c784abee8e2fc0b41a96e00dec
Hochia <johnny30618@gmail.com>
2021-01-04 22:45:08 +0800

    use presampling to train the data

--- Coding/cnn/no_beard.py
+++ Coding/cnn/no_beard.py
@@ -38,6 +38,9 @@ img_hw=(36,26)
 input_shape=(*img_hw, 3)
 # Load models
 del candidateModels
+nameModel=['C'+str(math.floor(i/2+1))+str(i%2+1)+'D'+str(math.floor(j/2+1))+str(j%2+1) for i in range(0,6) for j in range(0,6)]
+cnnMethods=[method for method in dir(CnnLys)[-6:]]
+dnnMethods=[method for method in dir(DnnLys)[-6:]]
 candidateModels=[getModels(getattr(CnnLys, layers[0]), getattr(DnnLys, layers[1]), OpLy.lydSfm, name, input_shape) for layers,name in zip([(i,j) for i in cnnMethods for j in dnnMethods], nameModel)]
 # Model fit
 attr=attrs[24]
@@ -45,13 +48,16 @@ attr=attrs[24]
 root_model_save='Coding/data/model_sampling/'+attr+'_36_26'
 # Load data
 y=cnnAttr(df_attr, attr)
+# Pre sampling
+# pre_sampling
+# Training
 for model, name in zip(candidateModels, nameModel):
     model.compile(
         optimizer=keras.optimizers.SGD(),
         loss=keras.losses.CategoricalCrossentropy(from_logits=True),
         metrics=[keras.metrics.CategoricalAccuracy()]
     )
-    itrSamplingModel(iteration, root_model_save, name, model, x, y, sampling_size, batch_size, epochs)
+    itrSamplingModel(iteration, root_model_save, name, model, x, y, pre_sampling, batch_size, epochs)
     gc.collect()
     gc.garbage
 
